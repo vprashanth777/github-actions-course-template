7fc60856dd4a12563142a4aba543d7fe5337264c
Prashanth Veldandi <PVeldandi@portware.com>
2021-08-10 18:17:50 +0530

    fixed

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,15 +1,4 @@
 const Game = require('../src/game').default
-const fs = require('fs')
-
-describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      expect(data).toMatchSnapshot()
-      done()
-    })
-  })
-})
 
 describe('Game', () => {
   let game, p1, p2
@@ -61,48 +50,6 @@ describe('Game', () => {
       expect(game.player).toBe('Salem')
       game.nextPlayer()
       expect(game.player).toBe('Nate')
-    })
-  })
-
-  describe('hasWinner', () => {
-    it('Wins if any row is filled', async () => {
-      for (let r = 0; r < game.board.length; r++) {
-        for (let c = 0; c < game.board[r].length; c++) {
-          game.board[r][c] = 'X'
-        }
-        expect(game.hasWinner()).toBe(true)
-
-        for (let c = 0; c < game.board[r].length; c++) {
-          game.board[r][c] = null
-        }
-      }
-    })
-
-    it('Wins if any column is filled', async () => {
-      for (let r = 0; r < game.board.length; r++) {
-        for (let c = 0; c < game.board[r].length; c++) {
-          game.board[c][r] = 'X'
-        }
-        expect(game.hasWinner()).toBe(true)
-
-        for (let c = 0; c < game.board[r].length; c++) {
-          game.board[c][r] = null
-        }
-      }
-    })
-
-    it('Wins if down-left diagonal is filled', async () => {
-      for (let r = 0; r < game.board.length; r++) {
-        game.board[r][r] = 'X'
-      }
-      expect(game.hasWinner()).toBe(true)
-    })
-
-    it('Wins if up-right diagonal is filled', async () => {
-      for (let r = 0; r < game.board.length; r++) {
-        game.board[2 - r][r] = 'X'
-      }
-      expect(game.hasWinner()).toBe(true)
     })
   })
 })
